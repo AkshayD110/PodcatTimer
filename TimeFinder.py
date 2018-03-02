5c2e6902ff267f88604d1a4c13fda3a9e1147701
AkshayD110 <akshayd110@gmail.com>
2018-03-03 04:31:52 +0530

    changes made to handle larger files

--- TimeFinder.py
+++ TimeFinder.py
@@ -9,8 +9,15 @@ ifrunat1point5speed=lambda podcastTime: (podcastTime/1.5)
 toFinishWithinReachingOffice=lambda podcastTime: (podcastTime/timeTakenToReachOffice_inMins)
 
 whenRunAt1point5speed=ifrunat1point5speed(podcastTime)
-
 speedToRunAt=toFinishWithinReachingOffice(podcastTime)
+if(whenRunAt1point5speed>60):
+    timeInHours,timeInMins=divmod(whenRunAt1point5speed,60)
+    print(f"If run at 1.5X speed, it will take {timeInHours} Hours and  {timeInMins} minutes to complete")
+    print("You won't be able to finish it before reaching office !")
+else:
+    print(f"If run at 1.5X speed, it will take {whenRunAt1point5speed} minutes to complete")
+    print(f"To finish the podcast within reaching office, run it at {speedToRunAt}X pace")
 
-print(f"If run at 1.5X speed, it will take {whenRunAt1point5speed} minutes to complete")
-print(f"To finish the podcast within reaching office, run it at {speedToRunAt}X pace")
+
+
+
